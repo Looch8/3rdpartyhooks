d2508c6c18329ff2995ef77eeb59fa1a7fddb2c9
Luke Wheldale <lukewheldale@gmail.com>
2023-09-28 19:19:47 +0930

    feat: read local storage and set to state

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useLocalStorage } from "react-use";
+
+function App() {
+	const [storedMessage, setStoredMessage] = useLocalStorage("message", ""); // Save message to local storage
+	const [message, setMessage] = useState("");
+
+	// component is loaded
+	// gap of time
+	// component is rendered
+	// gap of time
+	// componentDidMount aka useEffect(() => {}, [])
+	// gap of time
+	// comononentWillUpdate aka useEffect(() => {}, [message])
+
+	useEffect(() => {
+		setMessage(storedMessage);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	return <div className="App">{message && <h1>{message}</h1>}</div>;
+}
+
+export default App;
